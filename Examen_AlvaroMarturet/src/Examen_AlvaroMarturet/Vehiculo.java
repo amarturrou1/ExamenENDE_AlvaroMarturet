13e9c60ab1abdf8465ab52e8b1eab06f73bd913a
amarturrou1 <amarturrou1@educacion.navarra.es>
2025-12-19 08:52:33 +0100

    metodos y tostring añadidos

--- Examen_AlvaroMarturet/src/Examen_AlvaroMarturet/Vehiculo.java
+++ Examen_AlvaroMarturet/src/Examen_AlvaroMarturet/Vehiculo.java
@@ -6,4 +6,52 @@ public class Vehiculo {
 	String modelo;
 	double preciohora;
 	//preciohora
+	
+	public Vehiculo(String matricula, String marca, String modelo, double preciohora) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.preciohora = preciohora;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public double getPreciohora() {
+		return preciohora;
+	}
+
+	public void setPreciohora(double preciohora) {
+		this.preciohora = preciohora;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", preciohora="
+				+ preciohora + "]";
+	}
+	
+	
 }
